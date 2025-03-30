503bc7beaea9bf4d835df4a4b5620d332c11084f
Rotimi <L00178193@atu.ie>
2025-03-30 19:55:04 +0100

    Setting up OBD onn  main PC

---
+++ obdTest.py
@@ -0,0 +1,14 @@
+import obd
+
+connection = obd.OBD("COM9") # auto connects to USB or RF
+
+cmd = obd.commands.SPEED
+
+respose = connection.query(cmd) # send command to obd 
+
+ports = obd.scan_serial()  
+
+
+print(ports)
+
+print(str(respose.value )+"\n") # returns   unit bearing 
