2ec7c9a928a716095c4299216e2f9570746402f1
Rotimi Awoniran <olarotimi@protonmail.com>
2025-04-19 21:17:15 +0100

    new enviroent testing

--- obdTest.py
+++ obdTest.py
@@ -1,10 +1,11 @@
+import time
 import obd
 
 connection = obd.OBD("COM9")  # auto connects to USB or RF
 
 cmd = obd.commands.SPEED
 
-respose = connection.query(cmd)  # send command to obd
+response = connection.query(cmd)  # send command to obd
 
 ports = obd.scan_serial()
 
@@ -12,7 +13,10 @@ print(ports)
 
 print("Tesing Hello World")
 
-print(str(respose.value) + "\n")  # returns   unit bearing
+while True:
+    respose = connection.query(cmd)
+    time.sleep(200)
+    print(str(respose.value) + "\n")  # returns   unit bearing
 
 port = connection
 
