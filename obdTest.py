c21f835a449ee277b3b9d5505ab54ad91c729078
Rotimi <L00178193@atu.ie>
2025-04-19 21:42:18 +0100

    high freqeuncy signal

--- obdTest.py
+++ obdTest.py
@@ -1,16 +1,16 @@
 import time
 import obd
 
+connection = obd.OBD("COM9")  # auto connects to USB or RF
+cmd = obd.commands.SPEED
+
 while True:
-    
-    connection = obd.OBD("COM9")  # auto connects to USB or RF
-    cmd = obd.commands.SPEED
     respose = connection.query(cmd)  # send command to obd
     ports = obd.scan_serial()
     print(ports)
     print("Tesing Hello World")
     respose = connection.query(cmd)
-    time.sleep(200)
+    time.sleep(2)
     print(str(respose.value) + "\n")  # returns   unit bearing
     port = connection
 
